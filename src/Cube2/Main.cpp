c07243454a80cec0caf5e1ce49635153b42951ab
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-11-11 16:13:18 +0200

    UPDATE

---
+++ src/Cube2/Main.cpp
@@ -0,0 +1,44 @@
+#include "../Base/Base.h"
+
+void main2()
+{
+    auto width = 400;
+    auto height = 400;
+
+    auto bitmap = make_unique<Bitmap>(width, height);
+
+    BitmapWindow1::Create(700, 100, width, height);
+
+    while (BitmapWindow1::Exists())
+    {
+        CheckFPS();
+        bitmap->Fill(BLACK);
+
+        auto time = (float)clock() / 40;
+        Camera camera = { 0, 0, 0 };
+        auto view = MatrixView(&camera);
+        Vector3 position = { 0, 0, 2 };
+        Vector3 rotation = { 0, time, 0 };
+        Vector3 scale = { 1, 1, 1 };
+        auto world = MatrixWorld(position, rotation, scale);
+        bitmap->DrawCube2(world * view);
+
+        bitmap->DrawBorder(GREEN);
+        BitmapWindow1::SetPixels(bitmap);
+        BitmapWindow1::Update();
+    }
+}
+
+int main()
+{
+    try
+    {
+        main2();
+    }
+    catch (const exception& e)
+    {
+        cerr << e.what() << endl;
+    }
+
+    return 0;
+}
