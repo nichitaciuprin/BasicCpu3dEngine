3167f2843864411f488181810d5acc4e01acaa61
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-11-12 16:04:07 +0200

    UPDATE

--- src/Triangle/Main.cpp
+++ src/Triangle/Main.cpp
@@ -18,48 +18,31 @@ void main2()
         Vector2Int p1;
         Vector2Int p2;
 
-        p0 = { 200, 50 };
-        p1 = { 150, 100 };
-        p2 = { 250, 150 };
+        // p0 = { 200, 50 };
+        // p1 = { 150, 100 };
+        // p2 = { 250, 150 };
+        // bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, GREEN);
 
-        bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, GREEN);
+        // p0 = { 100, 50 };
+        // p1 = { 150, 100 };
+        // p2 = { 50, 150 };
+        // bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, RED);
 
-        p0 = { 100, 50 };
-        p1 = { 150, 100 };
-        p2 = { 50, 150 };
-
-        bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, RED);
-
-        p0 = { 200, 250 };
-        p1 = { 200, 300 };
-        p2 = { 150, 350 };
-
-        bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, BLUE);
+        // p0 = { 200, 250 };
+        // p1 = { 200, 300 };
+        // p2 = { 150, 350 };
+        // bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, BLUE);
 
         p0 = { 200, 250 };
         p1 = { 200, 300 };
         p2 = { 250, 350 };
-
         bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, GREEN);
 
         // p0 = { 200, 200 };
         // p1 = { 250, 200 };
         // p2 = { 200, 250 };
-
         // bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, RED);
         // bitmap->ScreenSpaceDrawTriangle(p2, p1, p0, RED);
-
-        // p2.x = 160.207
-        // p2.y = 159.7
-        // p1.x = 160.207
-        // p1.y = 239.3
-        // p0.x = 240.005
-        // p0.y = 239.502
-        // p2 = { 150, 150 };
-        // p1 = { 150, 250 };
-        // p0 = { 250, 250 };
-
-        // bitmap->ScreenSpaceDrawTriangle(p0, p1, p2, GREEN);
 
         bitmap->DrawBorder(GREEN);
         BitmapWindow1::SetPixels(bitmap);
