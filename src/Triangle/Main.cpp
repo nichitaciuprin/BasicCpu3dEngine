deb709b1e2646460123fd2cbfea45ee5e0751b81
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-11-19 21:01:17 +0200

    UPDATE

--- src/Triangle/Main.cpp
+++ src/Triangle/Main.cpp
@@ -19,56 +19,65 @@ void main2()
         Vector2Int p2;
         Vector2Int p3;
 
-        // MAIN
-        {
-            p0 = { 250,  50 };
-            p1 = { 200, 100 };
-            p2 = { 300, 150 };
-            bitmap->DrawTriangle3(p0, p1, p2, RED);
+        // // MAIN
+        // {
+        //     p0 = { 250,  50 };
+        //     p1 = { 200, 100 };
+        //     p2 = { 300, 150 };
+        //     bitmap->DrawTriangle3(p0, p1, p2, RED);
 
-            p0 = { 150,  50 };
-            p1 = { 200, 100 };
-            p2 = { 100,  150 };
-            bitmap->DrawTriangle3(p0, p1, p2, GREEN);
+        //     p0 = { 150,  50 };
+        //     p1 = { 200, 100 };
+        //     p2 = { 100,  150 };
+        //     bitmap->DrawTriangle3(p0, p1, p2, GREEN);
+        // }
+
+        // // SQUARES
+        // {
+        //     p0 = {  0,  0 };
+        //     p1 = { 25,  0 };
+        //     p2 = { 25, 25 };
+        //     p3 = {  0, 25 };
+        //     bitmap->DrawTriangle3(p2, p3, p0, RED);
+        //     bitmap->DrawTriangle3(p0, p1, p2, GREEN);
+
+        //     p0 = {  0 + 50,  0 };
+        //     p1 = { 25 + 50,  0 };
+        //     p2 = { 25 + 50, 25 };
+        //     p3 = {  0 + 50, 25 };
+        //     bitmap->DrawTriangle3(p0, p3, p2, BLUE);
+        //     bitmap->DrawTriangle3(p1, p0, p2, YELLOW);
+        // }
+
+        // // BIRDS
+        // {
+        //     p0 = { 200, 250 };
+
+        //     p1 = { 200, 300 };
+        //     p2 = { 150, 350 }; bitmap->DrawTriangle3(p0, p1, p2, RED);
+        //     p2 = { 250, 350 }; bitmap->DrawTriangle3(p0, p2, p1, RED);
+
+        //     p1 = { 250, 250 };
+        //     p2 = { 300, 300 }; bitmap->DrawTriangle3(p1, p0, p2, GREEN);
+        //     p2 = { 300, 200 }; bitmap->DrawTriangle3(p1, p2, p0, GREEN);
+
+        //     p1 = { 150, 250 };
+        //     p2 = { 100, 200 }; bitmap->DrawTriangle3(p0, p1, p2, BLUE);
+        //     p2 = { 100, 300 }; bitmap->DrawTriangle3(p2, p0, p1, BLUE);
+
+        //     p1 = { 200, 200 };
+        //     p2 = { 150, 150 }; bitmap->DrawTriangle3(p0, p1, p2, MAGENTA);
+        //     p2 = { 250, 150 }; bitmap->DrawTriangle3(p0, p1, p2, MAGENTA);
+        // }
+
+        // ROMB
+        {
+            p1 = { 100,  50 };
+            p2 = {  25, 125 };
+            // p0 = {  50,  75 }; bitmap->DrawTriangle3(p0, p1, p2, ORANGE);
+            p0 = {  75, 100 }; bitmap->DrawTriangle3(p0, p1, p2, ORANGE);
         }
 
-        // SQUARES
-        {
-            p0 = {  0,  0 };
-            p1 = { 25,  0 };
-            p2 = { 25, 25 };
-            p3 = {  0, 25 };
-            bitmap->DrawTriangle3(p2, p3, p0, RED);
-            bitmap->DrawTriangle3(p0, p1, p2, GREEN);
-
-            p0 = {  0 + 50,  0 };
-            p1 = { 25 + 50,  0 };
-            p2 = { 25 + 50, 25 };
-            p3 = {  0 + 50, 25 };
-            bitmap->DrawTriangle3(p0, p3, p2, BLUE);
-            bitmap->DrawTriangle3(p1, p0, p2, YELLOW);
-        }
-
-        // BIRDS
-        {
-            p0 = { 200, 250 };
-
-            p1 = { 200, 300 };
-            p2 = { 150, 350 }; bitmap->DrawTriangle3(p0, p1, p2, RED);
-            p2 = { 250, 350 }; bitmap->DrawTriangle3(p0, p2, p1, RED);
-
-            p1 = { 250, 250 };
-            p2 = { 300, 300 }; bitmap->DrawTriangle3(p1, p0, p2, GREEN);
-            p2 = { 300, 200 }; bitmap->DrawTriangle3(p1, p2, p0, GREEN);
-
-            p1 = { 150, 250 };
-            p2 = { 100, 200 }; bitmap->DrawTriangle3(p0, p1, p2, BLUE);
-            p2 = { 100, 300 }; bitmap->DrawTriangle3(p2, p0, p1, BLUE);
-
-            p1 = { 200, 200 };
-            p2 = { 150, 150 }; bitmap->DrawTriangle3(p0, p1, p2, MAGENTA);
-            p2 = { 250, 150 }; bitmap->DrawTriangle3(p0, p1, p2, MAGENTA);
-        }
 
         bitmap->DrawBorder(GREEN);
         BitmapWindow1::SetPixels(bitmap);
