7cb62f37b94102e9557f8c97aabde0d9ad4c3c18
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-11-21 21:16:43 +0200

    UPDATE

--- src/DepthBuffer/Main.cpp
+++ src/DepthBuffer/Main.cpp
@@ -17,34 +17,6 @@ void main2()
         float zFar = 10;
         float zClose = 9;
 
-        // {
-        //     Vector3 p0 = { -1,  3,  zFar };
-        //     Vector3 p1 = {  0,  3,  zFar };
-        //     Vector3 p2 = {  2, -3,  zClose };
-        //     p0 /= 10;
-        //     p1 /= 10;
-        //     p2 /= 10;
-        //     bitmap->DrawTriangle1(p0, p1, p2, RED);
-        // }
-        // {
-        //     Vector3 p0 = {  3, -1,  zFar };
-        //     Vector3 p1 = {  3, -2,  zFar };
-        //     Vector3 p2 = { -3, -1,  zClose };
-        //     p0 /= 10;
-        //     p1 /= 10;
-        //     p2 /= 10;
-        //     bitmap->DrawTriangle1(p0, p1, p2, GREEN);
-        // }
-        // {
-        //     Vector3 p0 = { -2, -2,  zFar };
-        //     Vector3 p1 = { -3, -2,  zFar };
-        //     Vector3 p2 = {  1,  3,  zClose };
-        //     p0 /= 10;
-        //     p1 /= 10;
-        //     p2 /= 10;
-        //     bitmap->DrawTriangle1(p0, p1, p2, BLUE);
-        // }
-
         {
             Vector3 p0 = { -1,  3,  zFar };
             Vector3 p1 = {  0,  3,  zFar };
