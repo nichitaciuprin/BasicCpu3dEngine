0f474bbdcfddd50063eb52eb73b75efae9d72fdd
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-12-03 15:12:24 +0200

    UPDATE

--- src/Base/StateTriangle.h
+++ src/Base/StateTriangle.h
@@ -24,13 +24,13 @@ struct StateTriangle
 
         int x0 = (int)v0.x;
         int x1 = (int)v1.x;
-        int x2 = (int)v2.x;
+        // int x2 = (int)v2.x;
         int y0 = (int)v0.y;
         int y1 = (int)v1.y;
         int y2 = (int)v2.y;
         float z0 = v0.z;
         float z1 = v1.z;
-        float z2 = v2.z;
+        // float z2 = v2.z;
 
         dy = y2 - y0;
 
@@ -60,8 +60,6 @@ struct StateTriangle
 
         // int cross = lt0.dx * lt1.dy - lt0.dy * lt1.dx;
         // if (cross < 0)
-        // {
-        // }
     }
     void Update()
     {
@@ -69,29 +67,17 @@ struct StateTriangle
     }
     static void Update1(StateTriangle* obj)
     {
-        do { obj->lt0.Update(); } while (obj->y == obj->lt0.y0); obj->xl = obj->lt0.x0;
-        do { obj->lt1.Update(); } while (obj->y == obj->lt1.y0); obj->xr = obj->lt1.x0;
+        do { obj->lt0.Update(); } while (obj->y == obj->lt0.y0); obj->xl = obj->lt0.x0; obj->zl = obj->lt0.z;
+        do { obj->lt1.Update(); } while (obj->y == obj->lt1.y0); obj->xr = obj->lt1.x0; obj->zr = obj->lt1.z;
         obj->y++;
         if (obj->y == obj->lt1.y1)
             obj->step = &Update2;
     }
     static void Update2(StateTriangle* obj)
     {
-        do { obj->lt0.Update(); } while (obj->y == obj->lt0.y0); obj->xl = obj->lt0.x0;
-        do { obj->lt2.Update(); } while (obj->y == obj->lt2.y0); obj->xr = obj->lt2.x0;
+        do { obj->lt0.Update(); } while (obj->y == obj->lt0.y0); obj->xl = obj->lt0.x0; obj->zl = obj->lt0.z;
+        do { obj->lt2.Update(); } while (obj->y == obj->lt2.y0); obj->xr = obj->lt2.x0; obj->zr = obj->lt2.z;
         obj->y++;
     }
-    // void Update1()
-    // {
-    //     do { lt0.Update(); } while (y == lt0.y0); xl = lt0.x0;
-    //     do { lt1.Update(); } while (y == lt1.y0); xr = lt1.x0;
-    //     y++;
-    // }
-    // void Update2()
-    // {
-    //     do { lt0.Update(); } while (y == lt0.y0); xl = lt0.x0;
-    //     do { lt2.Update(); } while (y == lt2.y0); xr = lt2.x0;
-    //     y++;
-    // }
 };
 
