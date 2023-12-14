6c7570e3e27bfc102739cc8b68d91cf9b78927da
Nichita Ciuprin <nichita.ciuprin@feel-it-services.com>
2023-12-14 13:18:50 +0200

    UPDATE

--- src/Base/Math.h
+++ src/Base/Math.h
@@ -663,7 +663,63 @@ void ClipLineByZ5(Vector3& v0, Vector3& v1)
 {
     v0 += (v0 - v1) * v0.z / (v1.z - v0.z);
 }
+void ClipTriangleRight(Vector3& p0, Vector3& p1, Vector3& p2, Vector3& p4, int& outCode)
+{
+    int state = 0;
 
+    if (p2.x > 1) state += 1;
+    if (p1.x > 1) state += 2;
+    if (p0.x > 1) state += 4;
+
+    switch (state)
+    {
+        /* 000 */ case 0: { outCode = 0; return; };
+        /* 111 */ case 7: { outCode = 2; return; };
+        default: break;
+    }
+
+    outCode = 1;
+
+    Vector3 v0, v1, v2;
+
+    switch (state)
+    {
+        /* 001 */ case 1: { v0 = p2; v1 = p0; v2 = p1; goto POINT_1; };
+        /* 010 */ case 2: { v0 = p1; v1 = p2; v2 = p0; goto POINT_1; };
+        /* 100 */ case 4: { v0 = p0; v1 = p1; v2 = p2; goto POINT_1; };
+
+        /* 011 */ case 3: { v0 = p0; v1 = p1; v2 = p2; goto POINT_2; };
+        /* 101 */ case 5: { v0 = p1; v1 = p0; v2 = p2; goto POINT_2; };
+        /* 110 */ case 6: { v0 = p2; v1 = p0; v2 = p1; goto POINT_2; };
+
+        default: abort();
+    }
+
+    POINT_1:
+    {
+        Vector3 b1 = v0;
+        Vector3 b2 = v0;
+        b1 += (b1 - v1) * b1.z / (v1.z - b1.z);
+        b2 += (b2 - v2) * b2.z / (v2.z - b2.z);
+        p0 = b1;
+        p1 = b2;
+        p2 = v1;
+        p4 = v2;
+        return;
+    }
+
+    POINT_2:
+    {
+        // v1 += (v1 - v0) * v1.z / (v0.z - v1.z);
+        // v2 += (v2 - v0) * v2.z / (v0.z - v2.z);
+        // v0.z += nearZ;
+        // v1.z += nearZ;
+        // v2.z += nearZ;
+        // if (v0.z != 0) { v0.x /= v0.z; v0.y /= v0.z; };
+        // DrawTriangle2(v0, v1, v2, pixel);
+        return;
+    }
+}
 
 void ClipLineByZ3(Vector3& v0, Vector3& v1, bool& include)
 {
