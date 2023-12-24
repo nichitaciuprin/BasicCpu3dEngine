72217febb5c35670c4cfb1ed38c4e35e1164b25a
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-12-24 20:52:24 +0200

    UPDATE

--- src/Base/Models.h
+++ src/Base/Models.h
@@ -1,7 +1,5 @@
 #pragma once
 
-// float h = 0.5f;
-
 namespace Models
 {
     namespace Cube
@@ -17,22 +15,6 @@ namespace Models
             Vector3{  0.5f,  0.5f, -0.5f },
             Vector3{  0.5f,  0.5f,  0.5f }
         };
-
-        // int indicesLines[12][2] =
-        // {
-        //     0,1,
-        //     1,5,
-        //     5,4,
-        //     4,0,
-        //     2,3,
-        //     3,7,
-        //     7,6,
-        //     6,2,
-        //     0,2,
-        //     1,3,
-        //     5,7,
-        //     4,6
-        // };
     }
 }
 
