9d2cee1bc96a8e3de405330134100e4010245272
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-09-14 18:51:27 +0300

    UPDATE

--- src/Level/Main.cpp
+++ src/Level/Main.cpp
@@ -1,4 +1,35 @@
-#include "BaseOld.h"
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <memory>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <windows.h>
+#include <winuser.h>
+
+#include "Math2.h"
+#include "Models.h"
+#include "Clipping.h"
+#include "Time.h"
+#include "Subgen.h"
+#include "Physics.h"
+#include "Bitmap.h"
+#include "BitmapWindow.h"
 
 void main2()
 {
