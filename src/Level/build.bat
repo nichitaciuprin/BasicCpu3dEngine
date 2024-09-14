a1be256ffcf38835e52cada2da91a71815c919f4
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-09-14 19:46:58 +0300

    TEMP

--- src/Level/build.bat
+++ src/Level/build.bat
@@ -21,8 +21,9 @@ set LIB=%LIB%%deps%\WindowsKits\10\Lib\10.0.22621.0\ucrt\x64;
 set LINKER_FLAGS=/INCREMENTAL:NO
 set SYSTEM_LIBS=user32.lib gdi32.lib winmm.lib d3d11.lib d3dcompiler.lib
 
+set _link=%deps%\MSVC\bin\Hostx64\x64\link.exe
 set _cl=%deps%\MSVC\bin\Hostx64\x64\cl.exe
-set _output=/Fe"%build%\main" /Fo"%build%\main"
+
 set _optimimisationDisable=/Od
 set _optimimisationLevel2=/O2
 set _enableWarningsLevel4=/W4
@@ -30,6 +31,7 @@ set _treatWarningsAsErrors=/WX
 set _hidelogs=/nologo
 set _exceptionHandling=/EHsc
 set _enablesExtraWarning=/analyze
+
 set _options=^
 %_exceptionHandling% ^
 %_hidelogs% ^
@@ -39,6 +41,10 @@ set _options=^
 
 if not exist build mkdir build
 
-%_cl% main.cpp %_output% %_options% %LINKER_FLAGS% %SYSTEM_LIBS%
+%_cl% /nologo /c test.cpp /Fo%build%\test %_options%
+@REM %_cl% /nologo /c main.cpp /Fo"%build%\main" %_options%
+@REM %_link% /nologo "%build%\test.obj" "%build%\main.obj" /OUT:"%build%\main.exe" %LINKER_FLAGS% %SYSTEM_LIBS%
+
+@REM %_cl% main.cpp %_output% %_options% %LINKER_FLAGS% %SYSTEM_LIBS%
 
 if %errorlevel% neq 0 exit /b %errorlevel%
