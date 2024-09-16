370a2f3f330b417c04f86f5bd90e8d0d79e85dfa
Nichita Ciuprin <nichitaciuprin@gmail.com>
2024-09-16 19:53:06 +0300

    UPDATE

--- src/CubeWireframe/Main.cpp
+++ src/CubeWireframe/Main.cpp
@@ -1,4 +1,12 @@
-#include "../Base/Base.h"
+#include "Std.h"
+#include "StdExt.h"
+#include "SysHelper.h"
+#include "Subgen.h"
+#include "Helper.h"
+#include "Models.h"
+#include "Clipping.h"
+#include "Bitmap.h"
+#include "BitmapWindow.h"
 
 void main2()
 {
@@ -13,9 +21,9 @@ void main2()
         CheckFPS();
         bitmap->Fill(BLACK);
 
-        auto time = (float)clock() / 40;
+        auto time = (float)clock() / 400;
         Camera camera = { 0, 0, 0 };
-        auto view = MatrixView(camera);
+        auto view = MatrixView(&camera);
         Vector3 position = { 0, 0, 2 };
         Vector3 rotation = { 0, time, 0 };
         Vector3 scale = { 1, 1, 1 };
@@ -23,7 +31,7 @@ void main2()
         bitmap->DrawCubeWireframe(world * view, RED);
 
         bitmap->DrawBorder(GREEN);
-        window->SetPixels(bitmap);
+        window->SetPixels(bitmap->pixels.data(), bitmap->Width(), bitmap->Height());
         window->Update();
     }
 }
--- src/CubeWireframe/run.bat
+++ src/CubeWireframe/run.bat
@@ -2,5 +2,5 @@
 SETLOCAL
 set _projectroot=%~dp0
 
-%_projectroot%build\BoidsDirectX.exe
+%_projectroot%build\main.exe
 if %errorlevel% neq 0 echo === ERROR === %errorlevel%
